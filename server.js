2a399fbe47a4f6a946e32636e258b3772e6016cd
hector27 <hector27c7@gmail.com>
2020-04-24 19:45:25 -0700

    second week

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+const app = express();
+const connect = require('./config/db');
+dotenv.config({ path: './config/config.env' });
+
+app.use(cors());
+
+const PORT = process.env.PORT || 6000;
+
+connect();
+
+app.listen(PORT, () => {
+  console.log('Server is running');
+});
+
 var http = require('http');
 
 //create a server object:
@@ -6,4 +24,4 @@ http
     res.write('hello world'); //write a response to the client
     res.end(); //end the response
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(PORT); //the server object listens on port 8080
