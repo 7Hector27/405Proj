a03dbba37e3ef2f617465d5675fed9f2ccb122f8
hector27 <hector27c7@gmail.com>
2020-04-24 19:55:47 -0700

    second week

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ http
     res.write('hello world'); //write a response to the client
     res.end(); //end the response
   })
-  .listen(PORT); //the server object listens on port 8080
+  .listen(6000); //the server object listens on port 8080
